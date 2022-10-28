c1b085544fc4f1dde179d8670a605ff2c13d2ff2
Keitey <hkmdev22@gmail.com>
2022-10-28 09:31:26 -0300

    iniciando processo de validação

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,11 @@ import {
   FormLabel,
   Select,
   Button,
+  FormErrorMessage,
 } from "@chakra-ui/react";
-
 import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const App = () => {
   const [cities, setCities] = useState([]);
@@ -28,96 +30,197 @@ const App = () => {
     );
   }, []);
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      tel: "",
+      cpf: "",
+      pais: "",
+      cidade: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Nome obrigatório")
+        .min(3, "Nome muito curto"),
+      email: Yup.string()
+        .required("Email obrigatório")
+        .min(3, "Email necessário"),
+      tel: Yup.string()
+        .required("Telefone obrigatório")
+        .min(3, "Telefone necessário"),
+      cpf: Yup.string().required("CPF obrigatório"),
+      pais: Yup.string().required("País obrigatório"),
+      cidade: Yup.string().required("Cidade obrigatória"),
+    }),
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 6));
+      actions.resetForm();
+    },
+  });
+
   return (
-    <Box h="100vh">
-      <Center
-        as="header"
-        h={150}
-        bg="#F8DB1C"
-        color="#fefce8"
-        fontWeight="bold"
-        fontSize="4xl"
-        pb="8"
-      >
-        Viajar é bom demais!
-      </Center>
-      <Flex
-        align="center"
-        justify="center"
-        bg="blackAlpha.200"
-        h="calc(100vh - 150px)"
-      >
+    <>
+      <Box h="100vh">
         <Center
-          w="100%"
-          maxW={840}
-          bg="white"
-          top={100}
-          position="absolute"
-          borderRadius={5}
-          p="6"
-          boxShadow="0 1px 2px #ccc"
+          as="header"
+          h={150}
+          bg="#F8DB1C"
+          color="#fefce8"
+          fontWeight="bold"
+          fontSize="4xl"
+          pb="8"
         >
-          <FormControl display="flex" flexDir="column" gap="4">
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="nome">Nome Completo</FormLabel>
-                <Input id="nome" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input id="email" />
-              </Box>
-            </HStack>
-            <HStack spacing="4">
-              <Box w="100%">
-                <FormLabel htmlFor="tel">Telefone</FormLabel>
-                <Input id="tel" type="number" />
-              </Box>
-              <Box w="100%">
-                <FormLabel htmlFor="cpf">CPF</FormLabel>
-                <Input id="cpf" type="number" />
-              </Box>
-            </HStack>
-            <HStack spacing="4">
-              <Box w="50%">
-                <FormLabel htmlFor="pais">País</FormLabel>
-                <Select placeholder="Selecione o país">
-                  {countries.map((country) => (
-                    <option key={country.code} value={country.code}>
-                      {country.name_ptbr}
-                    </option>
-                  ))}
-                </Select>
-              </Box>
-              <Box w="50%">
-                <FormLabel htmlFor="cidade">Cidade</FormLabel>
-                <Select placeholder="Selecione a cidade">
-                  {cities.map((city) => (
-                    <option key={city.id} value={city.id}>
-                      {city.name_ptbr}
-                    </option>
-                  ))}
-                </Select>
-              </Box>
-            </HStack>
-            <HStack justify="center">
-              <Button
-                w={240}
-                p="6"
-                type="submit"
-                colorScheme="yellow"
-                color="white"
-                fontWeight="bold"
-                mt="2"
-                fontSize="xl"
-              >
-                Enviar
-              </Button>
-            </HStack>
-          </FormControl>
+          Viajar é bom demais!
         </Center>
-      </Flex>
-    </Box>
+        <Flex
+          align="center"
+          justify="center"
+          bg="blackAlpha.200"
+          h="calc(100vh - 150px)"
+        >
+          <Center
+            w="100%"
+            maxW={840}
+            bg="white"
+            top={100}
+            position="absolute"
+            borderRadius={5}
+            p="6"
+            boxShadow="0 1px 2px #ccc"
+            as="form"
+            onSubmit={formik.handleSubmit}
+          >
+            <FormControl display="flex" flexDir="column" gap="4">
+              <HStack spacing="4">
+                <FormControl
+                  isInvalid={formik.errors.name && formik.touched.name}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="name">Nome Completo</FormLabel>
+                    <Input
+                      id="name"
+                      name="name"
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                      onBlur={formik.handleBlur}
+                    />
+                    <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+                <FormControl
+                  isInvalid={formik.errors.email && formik.touched.email}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      name="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      onBlur={formik.handleBlur}
+                    />
+                    <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+              </HStack>
+              <HStack spacing="4">
+                <FormControl
+                  isInvalid={formik.errors.tel && formik.touched.tel}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="tel">Telefone</FormLabel>
+                    <Input
+                      id="tel"
+                      type="text"
+                      name="tel"
+                      onChange={formik.handleChange}
+                      value={formik.values.tel}
+                      onBlur={formik.handleBlur}
+                    />
+                    <FormErrorMessage>{formik.errors.tel}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+                <FormControl
+                  isInvalid={formik.errors.cpf && formik.touched.cpf}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="cpf">CPF</FormLabel>
+                    <Input
+                      id="cpf"
+                      type="text"
+                      name="cpf"
+                      onChange={formik.handleChange}
+                      value={formik.values.cpf}
+                      onBlur={formik.handleBlur}
+                    />
+                    <FormErrorMessage>{formik.errors.cpf}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+              </HStack>
+              <HStack spacing="4">
+                <FormControl
+                  isInvalid={formik.errors.pais && formik.touched.pais}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="pais">País</FormLabel>
+                    <Select placeholder="Selecione o país">
+                      {countries.map((country) => (
+                        <option
+                          key={country.code}
+                          name="pais"
+                          onChange={formik.handleChange}
+                          value={formik.values.pais}
+                          onBlur={formik.handleBlur}
+                        >
+                          {country.name_ptbr}
+                        </option>
+                      ))}
+                    </Select>
+                    <FormErrorMessage>{formik.errors.pais}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+                <FormControl
+                  isInvalid={formik.errors.cidade && formik.touched.cidade}
+                >
+                  <Box w="100%">
+                    <FormLabel htmlFor="cidade">Cidade</FormLabel>
+                    <Select placeholder="Selecione a cidade">
+                      {cities.map((city) => (
+                        <option
+                          key={city.id}
+                          name="cidade"
+                          onChange={formik.handleChange}
+                          value={formik.values.cidade}
+                          onBlur={formik.handleBlur}
+                        >
+                          {city.name_ptbr}
+                        </option>
+                      ))}
+                    </Select>
+                    <FormErrorMessage>{formik.errors.cidade}</FormErrorMessage>
+                  </Box>
+                </FormControl>
+              </HStack>
+              <HStack justify="center">
+                <Button
+                  w={240}
+                  p="6"
+                  type="submit"
+                  colorScheme="yellow"
+                  color="white"
+                  fontWeight="bold"
+                  mt="2"
+                  fontSize="xl"
+                >
+                  Enviar
+                </Button>
+              </HStack>
+            </FormControl>
+          </Center>
+        </Flex>
+      </Box>
+    </>
   );
 };
 
